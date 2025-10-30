cfe9e8b163d2d35c9950c6b61576f2b3068e35df
SaiPhanindra45 <phanindrapersonal02@gmail.com>
2025-10-30 23:29:27 +0530

    Fixed the marker icon

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import './index.css'
 
 import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import markerIcon2x from './public/images/marker-icon-2x.png';
+import markerIcon from './public/images/marker-icon.png';
+import markerShadow from './public/images/marker-shadow.png';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
