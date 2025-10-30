ba5c9b6fc1f8d17c3f5f00546651e85809916300
SaiPhanindra45 <phanindrapersonal02@gmail.com>
2025-10-31 00:37:09 +0530

    Fix Leaflet icon paths for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: '/marker-icon-2x.png',
   iconUrl: '/marker-icon.png',
+  shadowUrl: '/marker-shadow.png',
 });
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
